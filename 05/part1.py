454ae16bfec745dd7ade93e88f1a62815fbd5d7b
Steven Miclette <broham113@gmail.com>
2017-12-23 22:06:13 -0800

    Day 5 update

---
+++ 05/part1.py
@@ -0,0 +1,21 @@
+
+with open('input') as input_file:
+    instruction_array = []
+    for line in input_file:
+        instruction_array.append(int(line.strip()))
+
+    index = 0
+    num_steps = 0
+    while index < len(instruction_array):
+        # Since our list is numbers, there's an easy way to swap these two values with math instead of a temp varaible
+        # But, really, that saves us so little to be confusing...
+        t_index = index
+        index += instruction_array[index]
+        instruction_array[t_index] += 1
+
+        # And don't forget to count the number of steps!
+        num_steps += 1
+    # end while
+#end with
+
+print(num_steps)
